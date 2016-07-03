8b16cc251cc8e6120ee027f45397b2d777bb863d
Chris Umbel <chris@chrisumbel.com>
2016-07-03 00:47:17 -0400

    log only when debug

--- logging.c
+++ logging.c
@@ -5,12 +5,16 @@
 #include <xc.h>
 
 void log_init() {
+#ifdef __DEBUG    
     TRISC6 = 0;
     OpenUSART(USART_TX_INT_ON & USART_ASYNCH_MODE & USART_EIGHT_BIT & USART_BRGH_HIGH, 51);
+#endif
 }
 
 void log_message(char *s) {
+#ifdef __DEBUG    
     putrsUSART(s);
+#endif
 }
 
 void log_message_ln(char *s) {
